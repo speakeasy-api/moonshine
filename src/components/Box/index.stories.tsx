996b37caacce874401778366fbb6a7412b0bc5b6
Adam Bull <adam@speakeasy.com>
2024-10-21 13:07:31 +0100

    chore: setup eslint

--- src/components/Box/index.stories.tsx
+++ src/components/Box/index.stories.tsx
@@ -1,16 +1,16 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from '@storybook/react'
 
-import { Box } from ".";
+import { Box } from '.'
 
 const meta: Meta<typeof Box> = {
   component: Box,
-};
+}
 
-export default meta;
-type Story = StoryObj<typeof Box>;
+export default meta
+type Story = StoryObj<typeof Box>
 
 export const Primary: Story = {
   args: {
     children: <div>Children</div>,
   },
-};
+}
