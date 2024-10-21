aafc98abe3e492fcfb5d86bce0fec508ce842dbf
Adam Bull <adam@speakeasy.com>
2024-10-21 23:03:58 +0100

    feat: distribute css

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import styles from './index.module.css'
 interface BoxProps {
   children: React.ReactNode
 }
 
 export function Box({ children }: BoxProps) {
-  return <div style={{ border: '1px solid red' }}>{children}</div>
+  return <div className={styles.box}>{children}</div>
 }
