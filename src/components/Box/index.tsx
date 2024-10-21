1b9058554b7312e430025160a85fec145c5411ab
Adam Bull <adam@speakeasy.com>
2024-10-21 22:30:54 +0100

    feat: add vars to storybook

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import styles from './index.module.css'
+
 interface BoxProps {
   children: React.ReactNode
 }
 
 export function Box({ children }: BoxProps) {
-  return <div className={styles.box}>{children}</div>
+  return <div style={{ border: '1px solid red' }}>{children}</div>
 }
