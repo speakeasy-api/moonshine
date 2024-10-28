69a7a0507fb339c7416e4df1a001f55b2d1437f8
Adam Bull <adam@speakeasy.com>
2024-10-28 11:34:32 +0000

    chore: update modes

--- .storybook/modes.ts
+++ .storybook/modes.ts
@@ -3,8 +3,12 @@ import { themes } from '@storybook/theming'
 export const allModes = {
   light: {
     theme: themes.light,
+    backgrounds: {
+      value: '#fff',
+    },
   },
   dark: {
     theme: themes.dark,
+    backgrounds: { value: '#1E293B' },
   },
 }
