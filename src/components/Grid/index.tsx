8524f203fba434306999027fcc68f4c4ca6d948b
Adam Bull <adam@speakeasy.com>
2024-11-06 12:08:45 -0800

    chore: comment

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -57,8 +57,22 @@ interface GridProps {
   wrap?: boolean
 
   /**
-   * The padding of the grid.
-   * @default 0
+   * Can be an object of responsive Padding values, or just Padding values.
+   *
+   * @example Simple Padding
+   * padding: 10
+   *
+   * @example Responsive Padding
+   * padding: { sm: 10, md: 20, lg: 30, xl: 40 }
+   *
+   * @example Padding per side
+   * padding: { top: 10, right: 0, bottom: 10, left: 0 }
+   *
+   * @example Responsive Padding per side (just x and y axis)
+   * padding: { sm: 0, md: 0, lg: 0, xl: { x: 10, y: 12 } }
+   *
+   * @example Responsive Padding per side with different values for each side
+   * padding: { sm: 0, md: 0, lg: 0, xl: { top: 10, right: 0, bottom: 10, left: 0 } }
    */
   padding?: ResponsiveValue<Padding>
 }
