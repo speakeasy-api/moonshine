3bf44d8e1638990d131d10b61f83b3de6a3749b7
Adam Bull <adam@speakeasy.com>
2024-11-07 20:09:02 -0800

    chore: undo

--- src/components/Separator/index.tsx
+++ src/components/Separator/index.tsx
@@ -1,26 +1,17 @@
 import { cn } from '@/lib/utils'
-import { Orientation, Size } from '@/types'
+import { Orientation } from '@/types'
 
 interface SeparatorProps {
   orientation?: Orientation
-  yMargin?: Size
 }
 
-export function Separator({
-  orientation = 'horizontal',
-  yMargin = 'medium',
-}: SeparatorProps) {
+export function Separator({ orientation = 'horizontal' }: SeparatorProps) {
   return (
     <div
       className={cn(
         orientation === 'horizontal'
           ? 'bg-border h-[1px] w-full'
-          : 'bg-border h-full w-[1px]',
-        yMargin === 'small' && 'my-2',
-        yMargin === 'medium' && 'my-3',
-        yMargin === 'large' && 'my-4',
-        yMargin === 'xl' && 'my-5',
-        yMargin === '2xl' && 'my-6'
+          : 'bg-border h-full w-[1px]'
       )}
     />
   )
