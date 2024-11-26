b8f48c7c3049acfe9922413c64fc90109e8f6365
Adam Bull <adam@speakeasy.com>
2024-11-26 16:10:46 +0000

    chore: whoops

--- src/components/WorkspaceSelector/OrgSelector.tsx
+++ src/components/WorkspaceSelector/OrgSelector.tsx
@@ -166,7 +166,7 @@ export function OrgSelector({
           <div
             className={cn(
               'min-w-0 flex-1 truncate text-left text-lg font-semibold lowercase',
-              open ? 'text-white' : 'text-white/80'
+              open ? 'text-foreground' : 'text-foreground/80'
             )}
             style={{ width: getSelectorMedianWidth }}
           >
