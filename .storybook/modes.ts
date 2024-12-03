8d86ca0dd41e06c2376dae0bf5ab14a455704e69
Adam Bull <adam@speakeasy.com>
2024-12-03 14:29:04 +0000

    fix: modes

--- .storybook/modes.ts
+++ .storybook/modes.ts
@@ -10,4 +10,14 @@ export const allModes = {
     backgrounds: { value: '#fff' },
     theme: 'light',
   },
+  'dark desktop': {
+    backgrounds: { value: 'hsl(0, 0%, 7%)' },
+    theme: 'dark',
+    viewport: 'large',
+  },
+  'light desktop': {
+    backgrounds: { value: '#fff' },
+    theme: 'light',
+    viewport: 'large',
+  },
 }
