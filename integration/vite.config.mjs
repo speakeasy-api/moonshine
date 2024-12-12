41a80324e26e253823778ddf7bc28ffb1ac87230
Adam Bull <adam@speakeasy.com>
2024-12-12 12:08:45 +0000

    chore: integration env

--- integration/vite.config.mjs
+++ integration/vite.config.mjs
@@ -10,6 +10,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './setup.js',
   },
+  define: import.meta.env.VITEST ? {} : { global: 'window' },
   resolve: {
     alias: {
       react: path.resolve(__dirname, 'node_modules/react'),
