0009e7232a9a6f18ae2a4157592bdd376ea1da73
Matt Lalonde <51486667+mattlalonde@users.noreply.github.com>
2024-12-20 18:04:04 +0000

    add story to PageHeader

--- src/components/PageHeader/index.stories.tsx
+++ src/components/PageHeader/index.stories.tsx
@@ -1,6 +1,7 @@
 import { PageHeader } from '.'
 import { StoryObj, Meta } from '@storybook/react'
 import { Badge } from '../Badge'
+import { Facepile } from '../Facepile'
 
 const meta: Meta<typeof PageHeader> = {
   component: PageHeader,
@@ -34,6 +35,44 @@ export const Default: Story = {
 export const WithoutSubtitle: Story = {
   args: {
     title: 'Speakeasy',
+    image: 'https://avatars.githubusercontent.com/u/91446104?s=200&v=4',
+    children: [
+      <div className="mt-2">
+        <Badge variant="success">1.5m followers</Badge>
+      </div>,
+    ],
+  },
+}
+
+export const WithFacepileSubheader: Story = {
+  args: {
+    title: 'Speakeasy',
+    subtitle: (
+      <Facepile
+        avatars={[
+          {
+            name: 'Alice Johnson',
+            imageUrl: 'https://picsum.photos/id/10/200',
+          },
+          { name: 'Bob Smith', imageUrl: 'https://picsum.photos/id/20/200' },
+          {
+            name: 'Charlie Brown',
+            imageUrl: 'https://picsum.photos/id/30/200',
+          },
+          { name: 'Diana Ross', imageUrl: 'https://picsum.photos/id/40/200' },
+          {
+            name: 'Edward Norton',
+            imageUrl: 'https://picsum.photos/id/50/200',
+          },
+          { name: 'Fiona Apple', imageUrl: 'https://picsum.photos/id/60/200' },
+          {
+            name: 'George Clooney',
+            imageUrl: 'https://picsum.photos/id/70/200',
+          },
+        ]}
+        avatarSize="small"
+      />
+    ),
     image: 'https://avatars.githubusercontent.com/u/91446104?s=200&v=4',
     children: [
       <div className="mt-2">
