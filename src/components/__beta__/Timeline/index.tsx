ef42c0f8f2e59ee3249e6b2ff52b1ade4fecd6e1
Adam Bull <adam@speakeasy.com>
2025-01-19 13:55:23 +0000

    chore: working

--- src/components/__beta__/Timeline/index.tsx
+++ src/components/__beta__/Timeline/index.tsx
@@ -1,13 +1,7 @@
 import { Icon } from '@/components/Icon'
 import { IconName } from '@/components/Icon/names'
 import { cn } from '@/lib/utils'
-import {
-  PropsWithChildren,
-  Children,
-  isValidElement,
-  useState,
-  useEffect,
-} from 'react'
+import { PropsWithChildren, Children, isValidElement } from 'react'
 
 interface Position {
   y: number // pixels from top of page
@@ -25,58 +19,21 @@ interface ItemProps extends PropsWithChildren {
 const Item: React.FC<ItemProps> = ({
   position,
   iconName,
-  title,
   className,
   style,
 }) => {
-  // Force re-render on scroll
-  const [scrollY, setScrollY] = useState(0)
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollY(window.scrollY)
-    }
-
-    // Add scroll listener
-    window.addEventListener('scroll', handleScroll, { passive: true })
-
-    // Initial position
-    handleScroll()
-
-    // Cleanup
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
-
-  const [isHovered, setIsHovered] = useState(false)
-
-  const handleMouseEnter = () => {
-    setIsHovered(true)
-  }
-
-  const handleMouseLeave = () => {
-    setIsHovered(false)
-  }
-
   return (
     <div
       style={{
-        top:
-          ((position.y - scrollY) / window.innerHeight) * window.innerHeight -
-          scrollY / 2.5,
+        top: `${position.y}px`,
         ...style,
       }}
-      onMouseOver={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
       className={cn(
-        'bg-background relative flex size-auto min-h-12 min-w-12 scale-[0.85] cursor-pointer items-center justify-center rounded-full border p-2 transition-transform duration-500',
-        isHovered && 'border-primary/70 scale-100',
+        'bg-background absolute flex size-auto min-h-12 min-w-12 scale-[0.85] cursor-pointer items-center justify-center rounded-full border px-2 transition-transform duration-500',
         className
       )}
     >
       <Icon name={iconName} />
-      {position.y}
     </div>
   )
 }
@@ -96,11 +53,18 @@ const Root: React.FC<RootProps> = ({ children, className }) => {
   return (
     <div
       className={cn(
-        'fixed bottom-0 right-10 top-0 h-full w-auto min-w-10 rounded-2xl',
+        'absolute bottom-0 right-20 top-0 min-h-svh w-auto min-w-12 rounded-2xl',
         className
       )}
     >
-      <div className="h-full [mask-image:linear-gradient(to_bottom,transparent_0%,black_5%,black_90%,transparent_100%)] before:absolute before:left-1/2 before:-z-10 before:h-full before:w-[1px] before:-translate-x-1/2 before:bg-gradient-to-b before:from-transparent before:via-zinc-700 before:to-transparent">
+      <div
+        className="fixed bottom-0 top-0 z-10 h-full min-w-12"
+        style={{
+          backgroundImage:
+            'linear-gradient(to bottom, black 0%, transparent 7%, transparent 93%, black 100%)',
+        }}
+      ></div>
+      <div className="h-full before:absolute before:left-1/2 before:-z-0 before:h-full before:w-[1px] before:-translate-x-1/2 before:bg-gradient-to-b before:from-transparent before:via-zinc-600 before:to-transparent">
         {validChildren}
       </div>
     </div>
