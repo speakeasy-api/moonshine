2c9f303d668e7fe9d0cdff9a546889351e8fc67b
Matt Lalonde <51486667+mattlalonde@users.noreply.github.com>
2025-02-24 15:53:48 +0000

    feat: optional extra links in LoggedInUserMenu

--- src/components/LoggedInUserMenu/index.tsx
+++ src/components/LoggedInUserMenu/index.tsx
@@ -9,41 +9,54 @@ import {
 import { Stack } from '../Stack'
 import { UserAvatar } from '../UserAvatar'
 import { UserAvatarProps } from '../UserAvatar'
+import React, { Children, Fragment, ReactNode } from 'react'
+import { cn } from '@/lib/utils'
 
 interface LoggedInUserProps extends UserAvatarProps {
   email: string
+  children?: ReactNode | ReactNode[]
   onSignOut: () => void
 }
 
-export function LoggedInUserMenu({
+const Root: React.FC<LoggedInUserProps> = ({
   email,
   name,
   onSignOut,
   imageUrl,
   size = 'small',
-}: LoggedInUserProps) {
+  children,
+}) => {
+  const validChildren = Children.toArray(children).filter(
+    (child) => React.isValidElement(child) && child.type === MenuItem
+  )
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="cursor-pointer outline-none">
         <UserAvatar name={name} imageUrl={imageUrl} size={size} />
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="mt-2 max-w-64 p-0">
-        <LoggedInMenuContent name={name} email={email} onSignOut={onSignOut} />
+        <LoggedInMenuContent name={name} email={email} onSignOut={onSignOut}>
+          {validChildren}
+        </LoggedInMenuContent>
       </DropdownMenuContent>
     </DropdownMenu>
   )
 }
+Root.displayName = 'LoggedInUserMenu'
 
 interface LoggedInMenuContentProps extends Omit<LoggedInUserProps, 'imageUrl'> {
   email: string
+  children?: ReactNode | ReactNode[]
   onSignOut: () => void
 }
 
-function LoggedInMenuContent({
+const LoggedInMenuContent: React.FC<LoggedInMenuContentProps> = ({
   email,
   name,
+  children,
   onSignOut,
-}: LoggedInMenuContentProps) {
+}) => {
   return (
     <>
       <Stack padding={3}>
@@ -53,6 +66,12 @@ function LoggedInMenuContent({
         </div>
       </Stack>
       <DropdownMenuSeparator />
+      {Children.map(children, (child, index) => (
+        <Fragment key={index}>
+          {child}
+          <DropdownMenuSeparator />
+        </Fragment>
+      ))}
       <DropdownMenuItem
         onSelect={onSignOut}
         className="cursor-pointer px-3 py-2"
@@ -63,3 +82,29 @@ function LoggedInMenuContent({
     </>
   )
 }
+
+interface LoggedInMenuItemProps {
+  onSelect: () => void
+  children?: ReactNode | ReactNode[]
+  className?: string
+}
+
+const MenuItem: React.FC<LoggedInMenuItemProps> = ({
+  onSelect,
+  children,
+  className,
+}) => {
+  return (
+    <DropdownMenuItem
+      onSelect={onSelect}
+      className={cn('cursor-pointer px-3 py-2', className)}
+    >
+      {children}
+    </DropdownMenuItem>
+  )
+}
+MenuItem.displayName = 'LoggedInUserMenu.MenuItem'
+
+export const LoggedInUserMenu = Object.assign(Root, {
+  MenuItem,
+})
