4834ba758beb3185aeebe59374b91eafbdba81ad
Adam Bull <adam@speakeasy.com>
2025-06-19 15:32:51 +0100

    fix: undo

---
+++ src/components/Tabs/index.tsx
@@ -0,0 +1,87 @@
+import { cn } from '@/lib/utils'
+import React, { ReactElement, useEffect, useState } from 'react'
+
+export interface TabProps<I extends string> {
+  children: React.ReactNode
+  active?: boolean
+  id: I
+  onClick?: () => void
+  className?: string
+}
+
+const Tab = function Tab<I extends string>({
+  children,
+  active,
+  onClick,
+  className,
+}: TabProps<I>) {
+  return (
+    <div
+      className={cn(
+        'text-body mb-[-1px] cursor-pointer px-4 py-2 text-sm',
+        active &&
+          'bg-background text-foreground rounded-t-lg border-t border-r border-l',
+        className
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  )
+}
+
+export interface TabsProps<I extends string> {
+  children: Array<ReactElement<TabProps<I>>>
+  selectedTab: I
+  onTabChange: (identifier: I) => void
+  className?: string
+}
+
+type TypeWithDisplayName = {
+  displayName?: string
+}
+
+export function Tabs<I extends string>({
+  children,
+  selectedTab,
+  onTabChange,
+  className,
+}: TabsProps<I>) {
+  const [activeTab, setActiveTab] = useState<I>(
+    selectedTab || children[0].props.id
+  )
+
+  useEffect(() => {
+    setActiveTab(selectedTab || children[0].props.id)
+  }, [selectedTab])
+
+  const validChildren = React.Children.toArray(children).filter(
+    (child) =>
+      React.isValidElement(child) &&
+      (child.type as TypeWithDisplayName).displayName === 'Tabs.Tab'
+  ) as Array<React.ReactElement<TabProps<I>>>
+
+  return (
+    <div
+      className={cn(
+        'border-border flex flex-row justify-start gap-2 border-b select-none',
+        className
+      )}
+    >
+      {React.Children.map(validChildren, (child) => {
+        return React.cloneElement(child, {
+          ...child.props,
+          active: child.props.id === activeTab,
+          onClick: () => {
+            setActiveTab(child.props.id)
+            child.props.onClick?.()
+            onTabChange?.(child.props.id)
+          },
+        })
+      })}
+    </div>
+  )
+}
+
+Tab.displayName = 'Tabs.Tab'
+Tabs.Tab = Tab
