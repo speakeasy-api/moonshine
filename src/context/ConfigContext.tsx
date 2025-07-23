6ed7614c9f3f76314ce6b3b6f423a9b547d91258
mattlalonde <matt.ap.lalonde@gmail.com>
2025-07-23 15:34:50 +0100

    feat: update theme switcher

--- src/context/ConfigContext.tsx
+++ src/context/ConfigContext.tsx
@@ -1,18 +1,5 @@
-import { createContext, useCallback, useEffect } from 'react'
-import { atomWithStorage } from 'jotai/utils'
-import { useAtom } from 'jotai'
-
-export type Theme = 'light' | 'dark'
-const THEME_KEY = 'moonshine-theme'
-
-/*
- * TODO: remove the 'sandbox-theme' initial value after it has been implemented in the sand box for a while
- * The check is there to pick up old values in local storage as the storage key will be updated to 'moonshine-theme'
- */
-const appTheme = atomWithStorage<Theme | null>(
-  THEME_KEY,
-  localStorage.getItem('sandbox-theme') as Theme | null
-)
+import { createContext } from 'react'
+import { Theme } from './theme'
 
 export interface ConfigContextType {
   /**
@@ -36,8 +23,7 @@ export const ConfigContext = createContext<ConfigContextType | undefined>(
   undefined
 )
 
-export interface MoonshineConfigProviderProps
-  extends Pick<ConfigContextType, 'themeElement'> {
+export interface MoonshineConfigProviderProps extends ConfigContextType {
   children: React.ReactNode
 }
 
@@ -51,63 +37,9 @@ export interface MoonshineConfigProviderProps
 export function MoonshineConfigProvider({
   children,
   themeElement,
+  theme,
+  setTheme,
 }: MoonshineConfigProviderProps) {
-  const [theme, setTheme] = useAtom(appTheme)
-
-  // Set body class for theme
-  useEffect(() => {
-    themeElement.classList.toggle('dark', theme === 'dark')
-    themeElement.classList.toggle('light', theme === 'light')
-  }, [theme])
-
-  const handleThemeChange = useCallback(
-    (newTheme: Theme) => {
-      setTheme(newTheme)
-    },
-    [setTheme]
-  )
-
-  useEffect(() => {
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        if (mutation.attributeName === 'class') {
-          if (themeElement.classList.contains('dark') && theme !== 'dark') {
-            setTheme('dark')
-          } else if (
-            themeElement.classList.contains('light') &&
-            theme !== 'light'
-          ) {
-            setTheme('light')
-          }
-        }
-      })
-    })
-
-    observer.observe(themeElement, {
-      attributes: true,
-      attributeFilter: ['class'],
-    })
-
-    return () => observer.disconnect()
-  }, [])
-
-  useEffect(() => {
-    // Only run OS preference logic if there is NO theme in localStorage
-    const stored = localStorage.getItem(THEME_KEY)
-    if (stored) return // User has set a theme, do nothing
-
-    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
-    handleThemeChange(mediaQuery.matches ? 'dark' : 'light')
-
-    const handleChange = (e: MediaQueryListEvent) => {
-      handleThemeChange(e.matches ? 'dark' : 'light')
-    }
-    mediaQuery.addEventListener('change', handleChange)
-    return () => mediaQuery.removeEventListener('change', handleChange)
-  }, [handleThemeChange])
-
-  if (!theme) return null
-
   return (
     <ConfigContext.Provider value={{ themeElement, theme, setTheme }}>
       {children}
