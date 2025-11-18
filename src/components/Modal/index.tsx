4e4fe75ac7b18d633878bf135826e8552c9ce251
Adam Bull <adam@speakeasy.com>
2025-11-18 10:13:00 +0000

    fix: custom modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -15,9 +15,14 @@ import { Icon } from '../Icon'
 export interface ModalProps {
   closable?: boolean
   className?: string
+  layout: 'default' | 'custom'
 }
 
-export const Modal = ({ closable = false, className }: ModalProps) => {
+export const Modal = ({
+  closable = false,
+  className,
+  layout = 'default',
+}: ModalProps) => {
   const { screens, currentIndex, isOpen, close } = useModal()
 
   const currentScreen = screens[currentIndex]
@@ -29,34 +34,61 @@ export const Modal = ({ closable = false, className }: ModalProps) => {
     <Dialog open={isOpen} onOpenChange={closable ? close : undefined}>
       <DialogPortal>
         <DialogOverlay className="bg-surface-secondary fixed top-0 z-10 h-screen w-screen opacity-85" />
-        <DialogContent
-          className={cn(
-            'bg-surface-primary border-neutral-default fixed top-1/2 left-1/2 z-20 flex h-auto max-h-[85vh] min-h-[40vh] w-[90vw] max-w-[800px] -translate-x-1/2 -translate-y-1/2 flex-col gap-3 overflow-y-auto rounded-md p-10 shadow-lg outline-none',
-            className
-          )}
-        >
-          {closable && (
-            <DialogClose asChild>
-              <IconButton
-                variant="tertiary"
-                icon={
-                  <Icon
-                    name="x"
-                    className="text-neutral-default hover:text-neutral-default/80 size-7 focus:outline-none"
-                  />
-                }
-                aria-label="Close modal"
-                className="absolute top-4 right-4 focus:ring-0 focus:outline-none focus-visible:ring-0"
-              />
-            </DialogClose>
-          )}
-          {currentScreen.title && (
-            <DialogTitle className="text-display-sm">
-              {currentScreen.title}
-            </DialogTitle>
-          )}
-          <DialogDescription>{currentScreen.component}</DialogDescription>
-        </DialogContent>
+
+        {layout === 'default' ? (
+          <DialogContent
+            className={cn(
+              'bg-surface-primary border-neutral-default fixed top-1/2 left-1/2 z-20 flex h-auto max-h-[85vh] min-h-[40vh] w-[90vw] max-w-[800px] -translate-x-1/2 -translate-y-1/2 flex-col gap-3 overflow-y-auto rounded-md p-10 shadow-lg outline-none',
+              className
+            )}
+          >
+            {closable && (
+              <DialogClose asChild>
+                <IconButton
+                  variant="tertiary"
+                  icon={
+                    <Icon
+                      name="x"
+                      className="text-neutral-default hover:text-neutral-default/80 size-7 focus:outline-none"
+                    />
+                  }
+                  aria-label="Close modal"
+                  className="absolute top-4 right-4 focus:ring-0 focus:outline-none focus-visible:ring-0"
+                />
+              </DialogClose>
+            )}
+            {currentScreen.title && (
+              <DialogTitle className="text-display-sm">
+                {currentScreen.title}
+              </DialogTitle>
+            )}
+            <DialogDescription>{currentScreen.component}</DialogDescription>
+          </DialogContent>
+        ) : (
+          <DialogContent
+            className={cn(
+              'bg-surface-primary border-neutral-default fixed top-1/2 left-1/2 z-20 flex h-auto max-h-[85vh] min-h-[40vh] w-[90vw] max-w-[800px] -translate-x-1/2 -translate-y-1/2 flex-col gap-3 overflow-y-auto rounded-md p-10 shadow-lg outline-none',
+              className
+            )}
+          >
+            {closable && (
+              <DialogClose asChild>
+                <IconButton
+                  variant="tertiary"
+                  icon={
+                    <Icon
+                      name="x"
+                      className="text-neutral-default hover:text-neutral-default/80 size-7 focus:outline-none"
+                    />
+                  }
+                  aria-label="Close modal"
+                  className="absolute top-4 right-4 focus:ring-0 focus:outline-none focus-visible:ring-0"
+                />
+              </DialogClose>
+            )}
+            {currentScreen.component}
+          </DialogContent>
+        )}
       </DialogPortal>
     </Dialog>
   )
